9b4e6f59c6947fb5f1e6353c651dcfda267754cf
MaxR-3dprint <maxreichmann@gmx.net>
2025-02-21 16:47:44 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,14 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyCMwt-eSBsSm8TOhAPy-0uTbcE_yqkQZZY",
+    authDomain: "nfc-1-7a129.firebaseapp.com",
+    projectId: "nfc-1-7a129",
+    storageBucket: "nfc-1-7a129.firebasestorage.app",
+    messagingSenderId: "998146222147",
+    appId: "1:998146222147:web:cae43ba03cec87908eccf6"
+};
+
+// Initialisierung von Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
