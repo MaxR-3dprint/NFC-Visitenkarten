f750b70661705bc9a464534779ee1f8f02f03398
MaxR-3dprint <maxreichmann@gmx.net>
2025-02-21 17:04:46 +0100

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,1 +1,16 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+function registerUser() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        alert("✅ Registrierung erfolgreich!");
+    })
+    .catch((error) => {
+        alert("Fehler: " + error.message);
+    });
+}
 
